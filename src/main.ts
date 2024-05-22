02fbd5b5cf6254d3725bdc77705caf27df2bf446
Gabriel <gabriel.m.ledesma96@gmail.com>
2024-05-21 23:12:02 -0300

    production

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [process.env.URL_ALLOWED_1, process.env.URL_ALLOWED_2,'http://127.0.0.1:3001']
+    origin: '*',
   });
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
